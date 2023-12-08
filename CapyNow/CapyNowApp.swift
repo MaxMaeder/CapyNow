e4c57b573e555f907aef6d6516ce107b87e5c845
Max Maeder <max.s.maeder@gmail.com>
2023-12-08 13:57:49 -0600

    Initial Commit

---
+++ CapyNow/CapyNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapyNowApp.swift
+//  CapyNow
+//
+//  Created by Max Maeder on 12/8/23.
+//
+
+import SwiftUI
+
+@main
+struct CapyNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
